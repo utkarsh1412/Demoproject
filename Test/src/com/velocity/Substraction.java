b6f436dddfdb265ce9cc8298e7f159db9e83275a
utkarsh1412 <64246493+utkarsh1412@users.noreply.github.com>
2024-12-11 12:16:20 +0530

    substraction of two number

---
+++ Test/src/com/velocity/Substraction.java
@@ -0,0 +1,10 @@
+package com.velocity;
+
+public class Substraction {
+	 public static void main(String[] args) {
+			int x=10;
+			int y=20;
+			int z= x-y;
+			System.out.println("substraction is>> " + z);
+		}
+}
